c34e27ac04700a776445c31e7d2d48b6bbd379dc
tarcisiogsouza <117198474+tarcisiogsouza@users.noreply.github.com>
2022-11-01 16:04:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let minhaImagem = document.querySelector('img');
 
 minhaImagem.onclick = function() {
     let meuSrc = minhaImagem.getAttribute('src');
-    if(meuSrc === 'imagens/bronzodia.png') {
-      minhaImagem.setAttribute ('src','https://tarcisiogsouza.github.io/bronzodias/meme1.jpg');
+    if(meuSrc === 'https://tarcisiogsouza.github.io/bronzodias/bronzodia.png') {
+      minhaImagem.setAttribute (link href'src','https://tarcisiogsouza.github.io/bronzodias/meme1.jpg');
     } else {
-      minhaImagem.setAttribute ('src','imagens/bronzodia.png');
+      minhaImagem.setAttribute (link href 'src','https://tarcisiogsouza.github.io/bronzodias/bronzodia.png');
     }
 }
 let meuBotao = document.querySelector('button');
