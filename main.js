dbd38e66bba818da4eaddd043f8916c26c87dd62
tarcisiogsouza <117198474+tarcisiogsouza@users.noreply.github.com>
2022-11-01 16:14:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let minhaImagem = document.querySelector('img');
 
 minhaImagem.onclick = function() {
     let meuSrc = minhaImagem.getAttribute('src');
-    if(link href meuSrc === 'https://tarcisiogsouza.github.io/bronzodias/bronzodia.png') {
-      minhaImagem.setAttribute (link href 'src','https://tarcisiogsouza.github.io/bronzodias/meme1.jpg');
+    if(link ref meuSrc === 'https://tarcisiogsouza.github.io/bronzodias/bronzodia.png') {
+      minhaImagem.setAttribute (link ref 'src','https://tarcisiogsouza.github.io/bronzodias/meme1.jpg');
     } else {
-      minhaImagem.setAttribute (link href 'src','https://tarcisiogsouza.github.io/bronzodias/bronzodia.png');
+      minhaImagem.setAttribute (link ref 'src','https://tarcisiogsouza.github.io/bronzodias/bronzodia.png');
     }
 }
 let meuBotao = document.querySelector('button');
